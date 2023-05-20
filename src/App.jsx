766a891bdbdde788300398c656b280da19c98ecb
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-05-21 00:25:26 +0300

    #up 1.21.00

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,72 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { FiSettings } from 'react-icons/fi'
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import './App.css'
-
-import { Navbar, Footer, Sidebar, ThemeSettings, LineChart } from './components'
+import './App.css';
 
 import {
-  Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor
-} from './pages'
+  Navbar,
+  Footer,
+  Sidebar,
+  ThemeSettings,
+  LineChart,
+} from './components';
 
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from './pages';
+
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-
-  const activeMenu = true
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-            <TooltipComponent content="Settings" position='Top'>
-              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%' }}>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content='Settings' position='Top'>
+              <button
+                type='button'
+                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                style={{ background: 'blue', borderRadius: '50%' }}
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? (<div className='w-72 fixed dark:bg-secondary-dark-bg bg-white'><Sidebar /></div>) : (<div className='w-0 dark:bg-secondary-dark-bg'><Sidebar /></div>)}
-          <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen  w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
-          }>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full"><Navbar /></div>
+          {activeMenu ? (
+            <div className='w-72 fixed dark:bg-secondary-dark-bg bg-white'>
+              <Sidebar />
+            </div>
+          ) : (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen  w-full ${
+              activeMenu ? 'md:ml-72' : 'flex-2'
+            }`}
+          >
+            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              <Navbar />
+            </div>
           </div>
           <div>
             <Routes>
@@ -62,11 +98,10 @@ const App = () => {
               <Route path='/stacked' element={<Stacked />} />
             </Routes>
           </div>
-
         </div>
-      </BrowserRouter >
-    </div >
-  )
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,5 +1,4 @@
-import React, { createContext } from 'react'
-
+import React, { createContext, useContext, useState } from 'react';
 const StateContext = createContext();
 
 const initialState = {
@@ -7,16 +6,16 @@ const initialState = {
   cart: false,
   userProfile: false,
   notification: false,
-}
+};
 
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState(true)
+  const [activeMenu, setActiveMenu] = useState(true);
 
   return (
-    <StateContext.Provider value={{ activeMenu, }}>
+    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
       {children}
     </StateContext.Provider>
-  )
-}
+  );
+};
 
-export const useStateContext = () => useContext(StateContext)
+export const useStateContext = () => useContext(StateContext);
