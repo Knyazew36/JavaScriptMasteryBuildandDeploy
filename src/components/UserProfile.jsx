a1704f3616579ef0849b336390172532f165a3fc
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-05-21 10:59:00 +0300

    #up

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const UseProfile = () => {
-  return (
-    <div>UserProfile</div>
-  )
-}
+const UserProfile = () => {
+  return <div>UserProfile</div>;
+};
 
-export default UseProfile
+export default UserProfile;
